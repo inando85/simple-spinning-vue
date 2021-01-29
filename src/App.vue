66c37f65a6d97a015e580919cef61cc9d65035dd
Fernando Junior <fernando.junior85@outlook.com>
2021-01-29 01:22:15 -0300

    Created Spinning Loader

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  .app
+    SimpleSpinning
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import SimpleSpinning from './components/SimpleSpinning'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SimpleSpinning
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass" scoped>
+
+.app
+  width: 100%
+  height: 100vh
+  display: flex
+  justify-content: center
+  align-items: center
+
 </style>
---
+++ src/components/SimpleSpinning.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  .spinning
+</template>
+
+<script>
+export default {
+  name: 'SimpleSpinning',
+}
+</script>
+
+<style lang="sass" scoped>
+
+.spinning
+  width: 100px
+  height: 100px
+  border: 3px solid #000
+  border-top: 3px solid #84E
+  border-radius: 50%
+  animation: spinning 1s linear infinite
+
+@keyframes spinning
+  100%
+    transform: rotate(360deg)
+
+</style>
